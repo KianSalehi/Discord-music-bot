6ea73c4a0fe1981e401e4459eefedbfcdf194b44
Kian Salehi <kian.k.salehi@gmail.com>
2023-05-08 12:03:57 -0400

    first Commit

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const { AudioPlayer } = require('./audioPlayer.js')
+const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.Guilds, Discord.IntentsBitField.Flags.GuildMessages, Discord.IntentsBitField.Flags.GuildVoiceStates] });
+const audioPlayer = new AudioPlayer();
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`)
+    })
+
+    client.on('message', async (message) => {
+        if (message.content.startsWith(";p play")) {
+            audioPlayer.playMusic(message);
+        }
+        if (message.content.startsWith(';p stop')) {
+            console.log(message);
+        }
+    })
 })
+console.log(process.env['DISCORD_TOKEN'])
+client.login(process.env.DISCORD_TOKEN);
 app.listen(process.env.PORT || 3000)
